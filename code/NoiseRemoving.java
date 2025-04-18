f1ad9b16d6b20728c9d5ec973f9aad3693b250ee
Alexandra Hutchby <alexandrahutchby@gmail.com>
2025-04-18 22:00:47 +1200

    created main function and testing - final commit

--- code/NoiseRemoving.java
+++ code/NoiseRemoving.java
@@ -1,7 +1,18 @@
 package code;
 
+import java.io.File;
+import javax.swing.JFileChooser;
+
 public class NoiseRemoving {
     public static void main(String[] args) {
-        
+        JFileChooser imageFileChooser = new JFileChooser(new File("."));
+        int stateImageFileChooser = imageFileChooser.showOpenDialog(null);
+
+        if(stateImageFileChooser == JFileChooser.APPROVE_OPTION){
+            String fileName = imageFileChooser.getSelectedFile().getPath();
+            ImageProcess ip = new ImageProcess(fileName);
+            ip.cleanNoise();
+            ip.save("noise_removed.jpg");
+        }
     }
 }
