7078294aa05c35146eeb8e3f2a13cce8ae06a53a
Alexandra Hutchby <alexandrahutchby@gmail.com>
2025-04-18 21:34:54 +1200

    created the functions will move on and start image process

--- code/ImageProcess.java
+++ code/ImageProcess.java
@@ -1,5 +1,19 @@
 package code;
 
 public class ImageProcess{
-    
+    public ImageProcess(String image){
+
+    }
+
+    public void save(String imageName){
+
+    }
+
+    public void addNoise(float density){
+
+    }
+
+    public void cleanNoise(){
+        
+    }
 }
--- code/NoiseRemoving.java
+++ code/NoiseRemoving.java
@@ -1,5 +1,7 @@
 package code;
 
 public class NoiseRemoving {
-    
+    public static void main(String[] args) {
+        
+    }
 }
